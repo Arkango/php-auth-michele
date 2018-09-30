9660aea2cc5aaf71ad86b4f8d5b7011b7570a6d7
arkango <asaracino@aryma.it>
2018-09-30 20:36:45 +0200

    token class,lef delete , delete debug

---
+++ classes/PHPtoken.php
@@ -0,0 +1,101 @@
+    <?php
+    
+    require_once '../config.php';
+    
+    class PHPtoken {
+        
+        private static $dbh_internal;
+        
+        function __construct(){
+            global $dbh;
+            self::$dbh_internal = $dbh;
+        }
+        
+        public function checkToken($method = 'GET'){
+            
+            if($method != 'GET' && $method != 'POST'){
+                echo 'method invalid'; exit;
+            }
+            
+            $token = self::getToken();
+            
+            if(isset($token)){
+                if(!isset($_{$method}[$row['token']])){
+                    echo 'invalid token'; exit;
+                } 
+            }
+            
+            
+            
+        }
+        
+        public function getToken(){
+            
+            $ip = self::getRealIpAddr();
+            $query = self::$dbh_internal->prepare('SELECT token FROM phpauth_token WHERE ip = ? ORDER BY dt ASC LIMIT 1');
+            
+            try{
+                $query->execute(array($ip));
+                $row = $query->fetch(PDO::FETCH_ASSOC);
+            }catch(Exception $e ){
+                echo 'get token error'; exit;
+            }
+            
+            
+            if(isset($row['token'])){
+                return $row['token'];
+            }else{
+                 $token = self::createToken();
+                try{
+                    self::saveToken($token);
+                    return $token;
+                }catch(Exception $e ){
+                    echo 'save token method error'; exit;
+                }
+            }
+        }
+        
+        private function createToken(){
+            $length = 32;
+            $token = bin2hex(random_bytes($length));
+            return $token;
+        }
+        
+        private function saveToken($token){
+            
+            $query = self::$dbh_internal->prepare('INSERT INTO phpauth_tokens (token,ip) VALUES(?,?)');
+            $ip = self::getRealIpAddr();
+            try{
+                $query->execute(array($token,$ip));
+             
+                if(self::$dbh_internal->lastInsertId() == 0) 
+                    echo 'token not saved'; exit;
+                
+            }catch(Exception $e ){
+                echo 'saveToken error'; exit;
+            }
+            
+        }
+        
+        private function getRealIpAddr()
+        {
+            if (!empty($_SERVER['HTTP_CLIENT_IP']))   //check ip from share internet
+            {
+                $ip=$_SERVER['HTTP_CLIENT_IP'];
+            }
+            elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))   //to check ip is pass from proxy
+            {
+                $ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
+            }
+            else
+            {
+                $ip=$_SERVER['REMOTE_ADDR'];
+            }
+            return $ip;
+        }
+        
+        
+    }
+
+    $phptoken = new PHPtoken();
+    $phptoken->getToken();
--- index.php
+++ index.php
@@ -1,18 +1,17 @@
 <?php
 
 require_once 'vendor/autoload.php';
+require_once 'config.php';
 
 use PHPAuth\Config as PHPAuthConfig;
 use PHPAuth\Auth as PHPAuth;
-
-$dbh = new PDO("mysql:host=127.0.0.1;dbname=phpauth", "root", "arcangelo");
 
 $config = new PHPAuthConfig($dbh);
 $auth = new PHPAuth($dbh, $config);
 
 if (!$auth->isLogged()) {
     header('HTTP/1.0 403 Forbidden');
-    echo "Forbidden index file";
+    echo "Forbidden";
 
     exit();
 }
